f29af381b692ae3076f784e0353de6ae7bee575d
yashi8596 <deletio.422shinta@gmail.com>
2022-10-20 11:33:24 +0000

    [Finish]会員情報編集画面まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+
+  devise_for :customers,controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+    passwords: 'public/passwords'}
 
   namespace :admin do
     resources :items
     resources :customers, only:[:index, :show, :edit, :update]
   end
+
+  devise_for :admins,controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations',
+    passwords: 'admins/passwords'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

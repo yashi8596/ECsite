fac85306166f0fd32497b6c07e6e8a3e614a5a2a
yashi8596 <deletio.422shinta@gmail.com>
2022-10-23 19:51:31 +0000

    [Finished]ECサイト退会画面まで(管理者のレイアウト調整は時間がかかると判断したため後に回した)

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,18 @@
+class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.Order_detail.all
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(@order.id)
+      redirect_to admin_order_path(@order.id)
+
+    else
+      render :show
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,27 @@
 Rails.application.routes.draw do
 
-  devise_for :customers,controllers: {
-    sessions: 'public/sessions',
-    registrations: 'public/registrations',
-    passwords: 'public/passwords'}
+  devise_for :customers#,controllers: {
+    #sessions: 'public/sessions',
+    #registrations: 'public/registrations',
+    #passwords: 'public/passwords'}
+
+  scope module: :public do
+    root to: "homes#top"
+    get '/about' => 'homes#about'
+    get 'customers/confirm'
+    get 'customers/unsubscribe'
+    resources :customers, only:[:show, :edit, :update]
+  end
+
 
   namespace :admin do
-    resources :items
+
+    get '/' => 'homes#top'
+    resources :items, except:[:new]
     resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:show, :update]
+    resources :order_details, only:[:update]
+
   end
 
   devise_for :admins,controllers: {

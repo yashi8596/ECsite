422d820871fb1543f27a0433eaec45a16f6563d0
yashi8596 <deletio.422shinta@gmail.com>
2022-10-30 20:53:04 +0000

    [Finish]3.2.17(80%) 3.2.18(80%)まで完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,12 +1,46 @@
 class Public::OrdersController < Public::Base
   def new
-    @order = Order.new
-    @order.customer_id = current_customer.id
+    @order = current_customer.orders.new
+  end
+
+  def confirm
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+    @order.shipping_fee = 800
+    @order.total_payment = @total + @order.shipping_fee
+    @order = current_customer.orders.new(order_params)
+
+    if @order.invalid?
+      render :new
+    end
+  end
+
+  def success
+  end
+
+  def create
+    if @order.create!(order_params)
+      redirect_to success_order_path
+    else
+      render :confirm
+    end
   end
 
   def index
+    @orders = current_customer.orders.all
+    @order_details = current_customer.order_details.all
   end
 
   def show
+    @order = current_customer.orders.find(params[:id])
+    @order_detail = current_customer.order_details.all
+    @order.shipping_fee = 800
+    @total = @order_details.inject(0) { |sum, item| sum + item.sum_of_price }
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :postal_code, :address, :payment_method, :order_status)
   end
 end

74e78a81cfea801c93633982da7e36fd300e1034
yashi8596 <deletio.422shinta@gmail.com>
2022-11-13 17:15:52 +0000

    [Update]public/orders,items,cart_itemsのレイアウト調整

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,11 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :customer
 
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum order_status: { "入金待ち": 0, "入金確認": 1, "制作中": 2, "発送準備中": 3, "発送済み": 4 }
-  
+  enum order_status: { yet: 0, confirm: 1, working: 2, ready: 3, shipped: 4 }
+
 end
